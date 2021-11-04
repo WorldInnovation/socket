74da23aa42e116cf8ca9884fd114baba7a2dc90a
vvarankin <volodymyr.varankin@dxc.com>
2021-11-04 08:42:46 +0200

    client-server-thread-pool

---
+++ src/main/java/com/study/thread/ClientHandler.java
@@ -0,0 +1,72 @@
+package com.study.thread;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class ClientHandler implements Runnable
+{
+	public static final String ERROR_STOP_ECHO_SERVER_MESSAGE = "IOError in ClientHandler:";
+	private final Socket clientSocket;
+
+	public ClientHandler(Socket socket){
+		this.clientSocket = socket;
+	}
+
+	public void run()
+	{
+		System.out.println("Client handler run on server");
+
+		try(
+				Scanner scanner = new Scanner(clientSocket.getInputStream());
+				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+				)
+		{
+
+			while (scanner.hasNextLine()){
+
+				String echoMessage = scanner.nextLine();
+				//while((echoMessage = in.readLine()) != null)
+				//{
+				System.out.println("Client handler got message:" + echoMessage);
+				StringBuilder stringBuilder = new StringBuilder("echo: ${");
+				stringBuilder.append(echoMessage);
+				stringBuilder.append("}");
+				String echoServer = stringBuilder.toString() + "\n";
+				out.write(echoServer, 0, echoServer.length());
+				out.newLine();
+				out.flush();
+			}
+
+			//}
+		}
+
+	/*	try(
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		)
+		{
+			String echoMessage = in.readLine();;
+			//while((echoMessage = in.readLine()) != null)
+			//{
+				System.out.println("Client handler got message:" + echoMessage);
+				StringBuilder stringBuilder = new StringBuilder("echo: ${");
+				stringBuilder.append(echoMessage);
+				stringBuilder.append("}");
+				String echoServer = stringBuilder.toString() + "\n";
+				out.write(echoServer, 0, echoServer.length());
+				out.newLine();
+				out.flush();
+			//}
+
+		}*/
+		catch (IOException e)
+		{
+			//System.out.println(ERROR_STOP_ECHO_SERVER_MESSAGE + e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/main/java/com/study/thread/ServerThread.java
+++ src/main/java/com/study/thread/ServerThread.java
@@ -3,14 +3,15 @@ package com.study.thread;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class ServerThread
 {
 	public static final int SOCKET_PORT = 3000;
-	public static final String ERROR_STOP_ECHO_SERVER_MESSAGE = "IOError when close echo server:";
 
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
 		try (
 				ServerSocket serverSocket = new ServerSocket(SOCKET_PORT);
@@ -18,59 +19,22 @@ public class ServerThread
 		)
 		{
 			System.out.println("Server start on localhost:3000");
+			ExecutorService pool = Executors.newCachedThreadPool();
 			while (true)
 			{
-				try (
 						Socket socket = serverSocket.accept();
-				)
 				{
 					System.out.println("Server have got connection:" + socket.getInetAddress().getHostAddress());
-					//ClientHandler clientSocket = new ClientHandler(socket);
-					new Thread(new ClientHandler(socket)).start();
+					ClientHandler clientSocket = new ClientHandler(socket);
+					pool.execute(clientSocket);
 					System.out.println(socket.getInetAddress().getCanonicalHostName());
 				}
 			}
 		}
-	}
-
-	private static class ClientHandler implements Runnable
-	{
-		private final Socket clientSocket;
-
-		public ClientHandler(Socket socket){
-			this.clientSocket = socket;
-		}
-
-		public void run()
+		catch (IOException e)
 		{
-			System.out.println("Server run");
-
-			try(
-					BufferedReader	in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-					BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-			)
-			{
-				String echoMessage = null;
-				while((echoMessage = in.readLine()) != null)
-				{
-					System.out.println("Server got message:" + echoMessage);
-					StringBuilder stringBuilder = new StringBuilder("echo: ${");
-					stringBuilder.append(echoMessage);
-					stringBuilder.append("}");
-					String echoServer = stringBuilder.toString() + "\n";
-					out.write(echoServer, 0, echoServer.length());
-					out.newLine();
-					out.flush();
-				}
-
-			}
-			catch (IOException e)
-			{
-				System.out.println(ERROR_STOP_ECHO_SERVER_MESSAGE + e.getMessage());
-			}
-
+			e.printStackTrace();
 		}
-
 	}
 
 }
